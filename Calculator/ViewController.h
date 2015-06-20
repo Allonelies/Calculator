c37e599650b9899bbe88118ad7de66bc3730eef0
Allone <Allone@AllonedeMac-Pro.local>
2015-06-20 09:06:48 +0800

    Initial Commit

---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Allone on 15/6/20.
+//  Copyright (c) 2015年 Allone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Calculator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Calculator
+//
+//  Created by Allone on 15/6/20.
+//  Copyright (c) 2015年 Allone. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
